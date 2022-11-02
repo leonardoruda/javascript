886f3e84548132d86723cb66eae276be39f7b473
Leonardo Dums Ruda <114747593+leonardoruda@users.noreply.github.com>
2022-11-02 12:45:16 -0300

    Update script.js

--- aula12ex/exe015/script.js
+++ aula12ex/exe015/script.js
@@ -10,6 +10,7 @@ function verificar() {
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada: ${idade}`
         var genero = ''
+        var descr = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         img.style.width = 98
@@ -27,30 +28,38 @@ function verificar() {
             genero = "Homem"
             if (idade >=0 && idade < 10) {
                 img.setAttribute('src', '../imagens/babyboy.png')
+                descr = 'piazote'
             } else if (idade <21) {
                 img.setAttribute('src', '../imagens/menino.png')
+                descr = 'piá'
             } else if (idade < 52) {
                 img.setAttribute('src', '../imagens/homem.png')
+                descr = 'piazão'
             } else {
                 img.setAttribute('src', '../imagens/velho.png')
-
+                descr = 'véio'
             }
         } else {
             genero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 img.setAttribute('src', '../imagens/babygirl.png')
+                descr = 'pincesinha'
             } else if (idade <21) {
                 img.setAttribute('src', '../imagens/menina.png')
+                descr = 'menina'
             } else if (idade < 52) {
                 img.setAttribute('src', '../imagens/mulher.png')
+                descr = 'muié'
             } else {
                 img.setAttribute('src', '../imagens/velha.png')
+                descr = 'véia'
             }
         }
 
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos:`
         res.appendChild(img)
+        res.innerHTML += `"${descr.toUpperCase()}"`
         res.appendChild(teste)
         res.appendChild(button)
     }
