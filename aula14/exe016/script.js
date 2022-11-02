954afba88a049392e471e79b69bf0a2ef3f23dcf
Leonardo Dums Ruda <114747593+leonardoruda@users.noreply.github.com>
2022-11-02 16:43:40 -0300

    Update script.js

--- aula14/exe016/script.js
+++ aula14/exe016/script.js
@@ -8,15 +8,21 @@ var res = document.getElementById('res')
 function contar() {
     if (n1.value == 0 || n2.value == 0) {
         res.innerHTML = 'Preencha todos os campos!'
-    } else if (n3.value == 0) {
-        window.alert('Você não selecionou um salto! Considerando PASSO 1')
+    } else if (n3.value <= 0) {
+        window.alert('O salto não pode ser nulo ou negativo! Considerando PASSO 1')
         n3.value = 1
-    } else {
+    } else if (n1.value < n2.value) {
         res.innerHTML = 'Contando: <br>'
         for (var c = Number(n1.value); c <= Number(n2.value); c += Number(n3.value)) {
-            res.innerHTML += `${c} &copy; `
+        res.innerHTML += `${c} \u{1F449} `
         }
-        res.innerHTML += `FIM!`
+        res.innerHTML += `\u{1F3C1}`
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        for (var c = Number(n1.value); c >= Number(n2.value); c -= Number(n3.value)) {
+            res.innerHTML += `${c} \u{1F449} `
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
 
