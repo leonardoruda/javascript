e7e89608efc08c6b9af3187a5b688d686888deb0
Leonardo Dums Ruda <114747593+leonardoruda@users.noreply.github.com>
2022-11-02 13:12:15 -0300

    created ambiente

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+console.log('TESTE 1')
+var c = 1
+while (c<=6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+console.log('TESTE 2')
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c<=6)
+
+//console.log('Tudo bem?')
