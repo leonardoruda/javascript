6964647145bef9350784792b019449fb6595d570
Leonardo Dums Ruda <114747593+leonardoruda@users.noreply.github.com>
2022-11-04 18:26:24 -0300

    funções

---
+++ aula16/funcao04.js
@@ -0,0 +1,12 @@
+function fatorial(x) {
+    let f = 1
+    console.log(`Calculando o fatorial de ${x}:`)
+    for (let c = x; c>1; c--) {
+       f *= c
+       console.log(`${x} * ${c}`)
+       
+    }
+    return f
+}
+
+console.log(`O fatorial é ${fatorial(5)}`)
