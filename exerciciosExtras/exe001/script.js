fe04e1dcc9ae2967845f630fa13fa50fbe202f4f
Leonardo Dums Ruda <114747593+leonardoruda@users.noreply.github.com>
2022-11-07 18:20:59 -0300

    Update script.js

--- exerciciosExtras/exe001/script.js
+++ exerciciosExtras/exe001/script.js
@@ -1,8 +1,14 @@
 function sobrenome() {
     let nome = document.getElementById('itxt').value
-    let espaço = nome.indexOf(" ")
-    let sobreNome = nome.substring(espaço)
-    let caract = sobreNome.length-1
-    let res = document.getElementById('res')
-    res.innerHTML = `<p>O sobrenome ${sobreNome} tem ${caract} letras!</p>`
+    if (nome.length == 0) {
+        alert('Você não digitou nada!')
+    } else if (nome.indexOf(' ') === -1) {
+        alert('Nome e sobrenome são separados por um espaço!')
+    } else {
+        let espaço = nome.indexOf(" ")
+        let sobreNome = nome.substring(espaço)
+        let caract = sobreNome.length-1
+        let res = document.getElementById('res')
+        res.innerHTML = `<p>O sobrenome ${sobreNome} tem ${caract} letras!</p>`
+    }
 }
