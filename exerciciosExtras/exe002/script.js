867b974d628c1dca131e759c3e8c58c108197423
Leonardo Dums Ruda <114747593+leonardoruda@users.noreply.github.com>
2022-11-08 12:54:06 -0300

    mess

--- exerciciosExtras/exe002/script.js
+++ exerciciosExtras/exe002/script.js
@@ -4,7 +4,7 @@ form.setAttribute('oninput', 'output.innerHTML = n')
 let fn = document.getElementsByTagName('input')
 let n = []
 let p = document.querySelector('div > p')
-p.innerHTML = 'Vamos adicionar números à nossa matriz: '
+p.innerHTML = 'Vamos adicionar números à nossa matriz [3x2]: '
 let p2 = document.createElement('p')
 p.appendChild(p2)
 
@@ -14,17 +14,21 @@ let output = document.createElement('output')
 output.setAttribute('id', 'n')
 res.appendChild(output)
 
-function adicionar () {
-    
+function começar () {
+    let button = document.createElement('button')
+    button.setAttribute('onclick', 'adicionar()')
+    button.innerText = 'Adicionar'
     for (let l = 1; l <= 3; l++) {
         for (let c = 1; c <=2; c++) {
             p2.innerHTML += `Digite um número na posição ${l}, ${c}: `
             p2.appendChild(form)
-            
+            n[l, c] = (Number(fn.value))
+            p2.appendChild(button)
         }
-        n.push(5)
-        p2.appendChild(button)
-        
+        //n.push(5)
     }
-    //`
+}
+
+function adicionar () {
+
 }
