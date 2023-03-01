b22a6265e74eb6f55a2f2a0d34db731b63d99418
Leonardo Ruda <leonardorudaog@gmail.com>
2023-03-01 17:37:21 -0300

    Update mongo.ts

--- nodeJS/src/database/mongo.ts
+++ nodeJS/src/database/mongo.ts
@@ -6,10 +6,8 @@ mongoose.set('strictQuery', true);
 export const mongoConnect = async () => {
     try {
         console.log('Conectando ao MongoDb...')
-        await mongoose.connect(process.env.MONGO_URL as string, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(process.env.MONGO_URL as string, 
+            {useNewUrlParser: true, useUnifiedTopology: true});
         console.log('Conectado!')
     } catch (error) {
         console.log('Erro Conexão: ' , error)
